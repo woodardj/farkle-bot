bb32572fdebe44b4fca309a069567dcb9573c3e5
Jonathan Woodard <woodardj@gmail.com>
2018-02-26 12:40:26 -0700

    add some cheeky nonsense to wimpybot

--- wimp.rb
+++ wimp.rb
@@ -7,7 +7,7 @@ class WimpyBot < FarkleBot
 
   def act(turn)
     roll = turn.last_roll
-    
+
     # If we have a five we'll keep it
     keepers = [roll.index(5)] if roll.index(5)
 
@@ -18,7 +18,7 @@ class WimpyBot < FarkleBot
     triple_value = roll.group_by(&:itself).select { |k, v| v.size >= 3 }.keys.first
     keepers = roll.each_index.select{|i| roll[i] == triple_value}[0..2] unless triple_value.nil?
 
-    # Whew. That's enough excitement for one day.
+    # Whew. That's enough excitement for one day. Gonna pass to the next player.
     {:keep => keepers, :pass => true, :puts => true}
   end
 end
