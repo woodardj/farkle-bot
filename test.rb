ba5bfde11abbb1a486877ebe6d12827f537f5f34
Jonathan Woodard <woodardj@gmail.com>
2018-02-26 07:49:20 -0700

    allow zero of non-scoring dice to be drafted

--- test.rb
+++ test.rb
@@ -21,3 +21,10 @@ less_dice.length == dice_count ? good : bad
 ace = [1,1,1,1,1,1] # Perfect roll
 score = game.score(ace, [0, 1, 2, 3, 4, 5])
 score == 2000 ? good : bad
+
+# If I were unsmart and only drafted a few of the perfect-roll 1s
+score = game.score(ace, [0])
+score == 100 ? good : bad
+
+score = game.score(ace, [1, 3, 5])
+score == 1000 ? good : bad
