d878a85d10ed63075d4f088ed30dd736058d2f3f
Jonathan Woodard <woodardj@gmail.com>
2018-02-26 07:47:36 -0700

    allow zero of non-scoring dice to be drafted

--- farkle.rb
+++ farkle.rb
@@ -5,22 +5,19 @@ class System
 
   def score(roll, keeps)
     values = keeps.map{|n| roll[n]}
-    puts values
 
     total_score = 0
     accumulator = Hash[1,0,2,0,3,0,4,0,5,0,6,0]
-    puts accumulator
     values.each do |d|
-      puts d
       accumulator[d] += 1
     end
 
     [2,3,4,6].each do |rank|
-      if [3,6].include? accumulator[rank] # These die faces only score in sets of three.
+      if [0,3,6].include? accumulator[rank] # These die faces only score in sets of three.
         # 2, 2, 2 == 200
         # 3, 3, 3 == 300
         # etc...
-        total_score += (count / 3) * 100 * rank
+        total_score += (accumulator[rank] / 3) * 100 * rank
       else
         raise "Can't draft a #{rank} in non-triples"
       end
